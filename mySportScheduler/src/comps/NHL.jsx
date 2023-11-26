1bbd47136999e898a7c0bf93d02d5c171d917d5d
abdulrahman1121 <abdallrahmaelmi456@gmail.com>
2023-11-25 23:59:35 -0800

    tried to add a div box

--- mySportScheduler/src/comps/NHL.jsx
+++ mySportScheduler/src/comps/NHL.jsx
@@ -8,6 +8,28 @@ const NHL = () => {
         backgroundPosition: "center",
         width: "100%",
         height: "900px",
+        import React from "react";
+
+const DivBox = () => {
+          const boxStyle = {
+            width: "200px",
+            height: "200px",
+            backgroundColor: "lightblue",
+            border: "2px solid blue",
+            borderRadius: "8px",
+            textAlign: "center",
+            paddingTop: "20px",
+          };
+        
+          return (
+            <div style={boxStyle}>
+              <p>This is a div box</p>
+            </div>
+          );
+        };
+        
+        export default DivBox;
+        
       };
       return (
         <div style={divStyle}>
