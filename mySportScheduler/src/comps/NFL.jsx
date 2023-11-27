e650f038d149ba0f1f09288ea817ff8d208610b4
abdulrahman1121 <abdallrahmaelmi456@gmail.com>
2023-11-26 23:58:06 -0800

    added the second div box to MLB and NFL and NBA comps

--- mySportScheduler/src/comps/NFL.jsx
+++ mySportScheduler/src/comps/NFL.jsx
@@ -24,17 +24,33 @@ const NFL = () => {
         transform: "translate(-50%, -75%)",
       };
     
-    return (
-    <div style={divStyle}>
-        <h3>NFL</h3>
-
-    <div style={boxStyle}>
-        <p>This is a div box</p>
+      const box2Style = {
+        width: "200px",
+        height: "200px",
+        backgroundColor: "orange",
+        border: "2px solid green",
+        borderRadius: "5px",
+        textAlign: "center",
+        paddingTop: "20px",
+        position: "absolute",
+        top: "75%",
+        left: "50%",
+        transform: "translate(150%, -150%)",
+  };
+      return (
+      <div style={divStyle}>
+          <h3>NHL</h3>
           
+      <div style={boxStyle}>
+          <p>This is a div box</p>
+      
+      <div style={box2Style}>
+        <p>This is div for teams</p>
+          </div>
         </div>
-    </div>
-
-    );
-}
+      </div>
+      
+      );
+  }
 
 export default NFL;
