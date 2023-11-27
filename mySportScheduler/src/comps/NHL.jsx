d6d4012030117e906ed9b968452f41e6c7bdedc0
abdulrahman1121 <abdallrahmaelmi456@gmail.com>
2023-11-26 23:56:37 -0800

    added another div box for soccer and NHL

--- mySportScheduler/src/comps/NHL.jsx
+++ mySportScheduler/src/comps/NHL.jsx
@@ -12,7 +12,7 @@ const NHL = () => {
 
       
     const boxStyle = {
-        width: "300px",
+        width: "400px",
         height: "300px",
         backgroundColor: "lightblue",
         border: "2px solid blue",
@@ -22,15 +22,32 @@ const NHL = () => {
         position: "absolute",
         top: "75%",
         left: "50%",
-        transform: "translate(-50%, -75%)",
+        transform: "translate(-75%, -25%)",
   };
+
+    const box2Style = {
+      width: "200px",
+      height: "200px",
+      backgroundColor: "orange",
+      border: "2px solid green",
+      borderRadius: "5px",
+      textAlign: "center",
+      paddingTop: "20px",
+      position: "absolute",
+      top: "75%",
+      left: "50%",
+      transform: "translate(150%, -150%)",
+};
     return (
     <div style={divStyle}>
         <h3>NHL</h3>
         
     <div style={boxStyle}>
         <p>This is a div box</p>
-      
+    
+    <div style={box2Style}>
+      <p>This is div for teams</p>
+        </div>
       </div>
     </div>
     
