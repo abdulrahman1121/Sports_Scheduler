5e5137518ee36517dd14af293335f497fda7b0a4
Your Name <you@example.com>
2023-11-28 17:30:16 -0800

    i got the nba teams from the api and now the user can see the nba

--- mySportScheduler/src/comps/NBA.jsx
+++ mySportScheduler/src/comps/NBA.jsx
@@ -1,42 +1,28 @@
 import React from "react";
 import NBApic from "../images/NBA2.jpg";
 import { useState, useEffect } from "react";
-// const [nabInfo,setNbaInfoo] = useState([])
 const NBA = () => {
-    const divStyle = {
-        backgroundImage: `linear-gradient(rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5)), url(${NBApic})`,
-        backgroundSize: "cover",
-        backgroundPosition: "center",
-        width: "100%",
-        height: "900px",
-      };
+  const [nabInfo, setNbaInfoo] = useState([]);
+  const divStyle = {
+    backgroundImage: `linear-gradient(rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5)), url(${NBApic})`,
+    backgroundSize: "cover",
+    backgroundPosition: "center",
+    width: "100%",
+    height: "900px",
+  };
 
-    const boxStyle = {
-        width: "300px",
-        height: "300px",
-        backgroundColor: "lightblue",
-        border: "2px solid blue",
-        borderRadius: "5px",
-        textAlign: "center",
-        paddingTop: "20px",
-        position: "absolute",
-        top: "75%",
-        left: "50%",
-        transform: "translate(-50%, -75%)",
-      };
-    
-      const box2Style = {
-        width: "200px",
-        height: "200px",
-        backgroundColor: "orange",
-        border: "2px solid green",
-        borderRadius: "5px",
-        textAlign: "center",
-        paddingTop: "20px",
-        position: "absolute",
-        top: "75%",
-        left: "50%",
-        transform: "translate(150%, -150%)",
+  const box2Style = {
+    width: "200px",
+    height: "200px",
+    backgroundColor: "orange",
+    border: "2px solid green",
+    borderRadius: "5px",
+    textAlign: "center",
+    paddingTop: "20px",
+    position: "absolute",
+    top: "75%",
+    left: "50%",
+    transform: "translate(150%, -150%)",
   };
   useEffect(() => {
     const fetchNba = async () => {
