b6cef25de855375cfa190087dc266683f9cd2e1e
Your Name <you@example.com>
2023-11-29 02:16:17 -0800

    Saving changes to NFL.jsx

--- mySportScheduler/src/comps/NFL.jsx
+++ mySportScheduler/src/comps/NFL.jsx
@@ -1,58 +1,71 @@
 import React from 'react';
 import NFLpic from '../images/NFL.jpg'
-
-const NFL = () => {
-    const divStyle = {
-        backgroundImage: `linear-gradient(rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5)), url(${NFLpic})`,
-        backgroundSize: "cover",
-        backgroundPosition: "center",
-        width: "100%",
-        height: "900px",
-      };
-      
-    const boxStyle = {
-        width: "400px",
-        height: "300px",
-        backgroundColor: "lightblue",
-        border: "2px solid blue",
-        borderRadius: "5px",
-        textAlign: "center",
-        paddingTop: "20px",
-        position: "absolute",
-        top: "75%",
-        left: "50%",
-        transform: "translate(-75%, -25%)",
-      };
-    
-      const box2Style = {
-        width: "200px",
-        height: "200px",
-        backgroundColor: "red",
-        border: "2px solid yellow",
-        borderRadius: "5px",
-        textAlign: "center",
-        paddingTop: "20px",
-        position: "absolute",
-        top: "75%",
-        left: "50%",
-        transform: "translate(150%, -150%)",
+import { useState, useEffect } from "react";
+const NBA = () => {
+  const [nabInfo, setNbaInfoo] = useState([]);
+  const divStyle = {
+    backgroundImage: `linear-gradient(rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5)), url(${NFLpic})`,
+    backgroundSize: "cover",
+    backgroundPosition: "center",
+    width: "100%",
+    height: "900px",
   };
-  
 
-      return (
-      <div style={divStyle}>
-          <h3>NFL</h3>
-          
-      <div style={boxStyle}>
-          <p>This is a div box</p>
-      
-      <div style={box2Style}>
-        <p>This is div for teams</p>
-          </div>
-        </div>
-      </div>
-      
+  const box2Style = {
+    width: "200px",
+    height: "200px",
+    backgroundColor: "orange",
+    border: "2px solid green",
+    borderRadius: "5px",
+    textAlign: "center",
+    paddingTop: "20px",
+    position: "absolute",
+    top: "75%",
+    left: "50%",
+    transform: "translate(150%, -150%)",
+  };
+  useEffect(() => {
+    const fetchNba = async () => {
+      const res = await fetch(
+        "http://site.api.espn.com/apis/site/v2/sports/football/nfl/scoreboard"
       );
-  }
+      const data = await res.json();
+      if (data.events ) {
+        setNbaInfoo(data.events);
+      }
+      console.log(data);
+    };
+    fetchNba();
+  }, []);
 
-export default NFL;
+  return (
+    <div style={divStyle}>
+      <h3>NBA</h3>
+      <div className="container">
+        {nabInfo.map((teams, index) => (
+          <div key={index}>
+            <div className="boxStyle">
+              <img className="logo-nba"
+                src={teams.competitions[0].competitors[0].team.logo}
+                alt=""
+              />
+              <img className="logo-nba"
+                src={teams.competitions[0].competitors[1].team.logo}
+                alt=""
+              />
+            </div>
+            <h2>{teams.shortName}</h2>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default NBA;
+
+{
+  /* <div style={box2Style}>
+         <p>This is div for teams</p>
+       </div> */
+}
